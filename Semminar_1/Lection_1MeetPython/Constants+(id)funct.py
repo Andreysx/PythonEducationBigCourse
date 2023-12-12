e32c038a7b0f7ad2bfe0c523c8a7674eeff219df
Andreye <drontop1999@mail>
2023-12-12 16:56:31 +0300

    Lection 1 commit

---
+++ Semminar_1/Lection_1MeetPython/Constants+(id)funct.py
@@ -0,0 +1,21 @@
+# Дополнительных команд для создания констант в языке Python нет. Есть лишь
+# договорённость что константа — это переменная написанное прописными буквами.
+# MAX_COUNT = 1000
+# ZERO = 0
+# DATA_AFTER_DELETE = 'No data'
+# DAY = 60 * 60 * 24
+# Вернёмся к тому что в Python всё объект, а переменная является ссылкой на объект.
+# Воспользуемся встроенной функцией id(), которая возвращает адрес объекта в
+# оперативной памяти вашего компьютера.
+# a = 5
+# print(id(a))
+# a = "hello world"
+# print(id(a))
+# a = 42.0 * 3.141592 / 2.71828
+# print(id(a))
+# Keywords:
+# False, None, True, and, as, assert, async, await, break, class,
+# continue, def, del, elif, else, except, finally, for, from,
+# global, if, import, in, is, lambda, nonlocal, not, or, pass,
+# raise, return, try, while, with, yield.
+#
