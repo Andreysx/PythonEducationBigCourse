de2722bbd7102369bbc6e9e63f6ca89fa7d98fc9
Andrey <A@w.com>
2025-12-22 17:44:09 +0300

    update QuickSort_alg.py

--- Algotithms_and_Data_types_byAd/QuickSort_alg.py
+++ Algotithms_and_Data_types_byAd/QuickSort_alg.py
@@ -33,10 +33,8 @@ def quicksort_2(array: list):
         return array
     else:
         pivot = array[(len(array) - 1) // 2]  # Опорный элемент - центральный элемент
-        less = [i for i in array if
-                i < pivot]  # Подмассив элементов меньших опорного(применяется List comprehension)
-        greater = [i for i in array if
-                   i > pivot]  # Подмассив элементов больших опорного(применяется List comprehension)
+        less = [i for i in array if i < pivot]  # Подмассив элементов меньших опорного(применяется List comprehension)
+        greater = [i for i in array if i > pivot]  # Подмассив элементов больших опорного(применяется List comprehension)
         return quicksort(less) + [pivot] + quicksort(greater)
 
 
